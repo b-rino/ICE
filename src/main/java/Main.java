c5d273ba12481724ccb3183b94c8bac6de891d4b
Benjamin Rino <148062776+b-rino@users.noreply.github.com>
2024-12-10 09:20:59 +0100

    tirdag first push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 //TODO: HVIS DU FAILER LOGIN I FØRSTE FORSØG SKAL DU LOGGE IND TO GANGE EFTERFØLGENDE!!! DISPLAY FUNDS OG MEMBERSHIP STATUS
+//TODO: Husk modeller før aflevering
 
-//Metoder lavet og rettet i: readMediaData samt de nederste 6 metoder i DBC. displayMedia og buyMedia i MediaClient. buyMembership i UserClient
+
 
 public class Main {
     public static void main(String[] args) {
