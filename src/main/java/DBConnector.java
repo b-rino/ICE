cba8dc53ab1ce024322fddaa767e5474363d55cb
Benjamin Rino <148062776+b-rino@users.noreply.github.com>
2024-12-11 14:34:24 +0100

    oprydning i nogle klasser og metoder

--- src/main/java/DBConnector.java
+++ src/main/java/DBConnector.java
@@ -365,6 +365,7 @@ public class DBConnector {
         return personalMediaList;
     }
 
+        //TODO: Disse 3 metoder bruges ikke indtil videre og bør slettes hvis ikke de kommer i brug!
         /*public int getMovieID(String title) {
         String sql = "SELECT MovieID FROM Movies WHERE username = ?";
 
@@ -401,5 +402,35 @@ public class DBConnector {
             System.out.println(e.getMessage());
         }
         return ID;
+    }
+
+        public void saveMediaData(MediaItem media) {
+        String movieSql = "INSERT INTO Movies (title, releaseYear, category, rating, type) VALUES (?,?,?,?, 'movie')";
+        String seriesSql = "INSERT INTO Series (title, releaseYear, category, rating, season, episode, type) VALUES (?, ?, ?, ?, ?, ?, 'series')";
+
+        try (Connection conn = this.connect()) {
+            if (media instanceof Movie) {
+                try (PreparedStatement pstmt = conn.prepareStatement(movieSql)) {
+                    pstmt.setString(1, media.getTitle());
+                    pstmt.setInt(2, media.getReleaseYear());
+                    pstmt.setString(3, media.getCategory());
+                    pstmt.setFloat(4, media.getRating());
+                    pstmt.executeUpdate();
+                }
+            } else if (media instanceof Series) {
+                try (PreparedStatement pstmt = conn.prepareStatement(seriesSql)) {
+                    Series series = (Series) media;
+                    pstmt.setString(1, media.getTitle());
+                    pstmt.setInt(2, media.getReleaseYear());
+                    pstmt.setString(3, media.getCategory());
+                    pstmt.setFloat(4, media.getRating());
+                    pstmt.setInt(5, series.getSeason());
+                    pstmt.setInt(6, series.getEpisode());
+                    pstmt.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }*/
 }
