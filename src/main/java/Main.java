1af23fa3741af0b8435b9400ef30edc5fad56ba9
Benjamin Rino <148062776+b-rino@users.noreply.github.com>
2024-12-16 10:59:38 +0100

    Samling af alt lavet til kl 11 Mandag

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,8 @@ import javax.print.attribute.standard.Media;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-//TODO: HVIS DU FAILER LOGIN I FØRSTE FORSØG SKAL DU LOGGE IND TO GANGE EFTERFØLGENDE!!!
 //TODO: Husk modeller før aflevering
-//TODO: display balance ved køb
+
 
 
 public class Main {
